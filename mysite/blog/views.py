3feb3aa108faed9eeebe22579efc111ec32b1229
safuente <santiagoafu@ext.inditex.com>
2024-07-23 08:58:20 +0200

    Added class view for listing posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,25 +1,16 @@
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.views.generic import ListView
 from django.shortcuts import get_object_or_404, render
 from .models import Post
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list,3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # If page number is not an integer get the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page number is out of range get last page of results
-        posts = paginator.page(paginator.num_pages)
-    return render(
-        request,
-        'blog/post/list.html',
-        {'posts': posts}
-    )
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
