c2621f5b9a70250b37270d4b4dc5bc6442c6dba8
safuente <santiagoafu@ext.inditex.com>
2025-02-10 21:01:09 +0100

    Added comment system

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,9 @@
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 from django.shortcuts import get_object_or_404, render
 from .models import Post
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 
 class PostListView(ListView):
@@ -24,10 +25,16 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day
     )
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
     return render(
         request,
         'blog/post/detail.html',
-        {'post': post}
+        {
+            'post': post,
+            'comments': comments,
+            'form': form
+         }
     )
 
 def post_share(request, post_id):
@@ -78,3 +85,32 @@ def post_share(request, post_id):
             'sent': sent
         },
     )
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    comment = None
+    # A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a Comment object without saving it to the database
+        comment = form.save(commit=False)
+        # Assign the post to the comment
+        comment.post = post
+        # Save the comment to the database
+        comment.save()
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment
+        },
+    )
+
