8cfa183d8c56d3f0c299d5a73dfd3e7d3d9dfe55
safuente <santiagoafu@ext.inditex.com>
2025-03-03 17:09:54 +0100

    Added tags filter

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -37,6 +38,7 @@ class Post(models.Model):
 
     objects = models.Manager()  # default manager
     published = PublishedManager()  # custom manager
+    tags = TaggableManager()
 
     class Meta:
         ordering = ['-publish']
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,19 +1,38 @@
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
-from django.views.generic import ListView
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
+from taggit.models import Tag
 from .models import Post
 from .forms import EmailPostForm, CommentForm
 
 
-class PostListView(ListView):
-    """
-    Alternative post list view
-    """
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    # Pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer get the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page_number is out of range get last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(
+        request,
+        'blog/post/list.html',
+        {
+            'posts': posts,
+            'tag': tag
+        }
+    )
+
 
 
 def post_detail(request, year, month, day, post):
