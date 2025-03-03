438586b0daa02195cd156855ca73576c1a70f9c5
safuente <santiagoafu@ext.inditex.com>
2025-03-03 17:19:24 +0100

    Added posts with similar tags

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Count
 from django.shortcuts import get_object_or_404, render
 from taggit.models import Tag
 from .models import Post
@@ -46,13 +47,23 @@ def post_detail(request, year, month, day, post):
     )
     comments = post.comments.filter(active=True)
     form = CommentForm()
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids
+    ).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+        same_tags=Count('tags')
+    ).order_by('-same_tags', '-publish')[:4]
     return render(
         request,
         'blog/post/detail.html',
         {
             'post': post,
             'comments': comments,
-            'form': form
+            'form': form,
+            'similar_posts': similar_posts
          }
     )
 
